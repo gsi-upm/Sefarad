62104c28eb6cabbd446888a4eabe8a5e8ad666ad
Alejandro <alejandro.saura.villanueva@gmail.com>
2015-02-19 18:58:24 +0100

    dc.js tests in progress

--- js/controller.js
+++ js/controller.js
@@ -19,7 +19,62 @@ $( document ).ready(function() {
     //configure widgets:
     initializeWidgets();
 
+
+    var data = [
+        {date: "12/27/2012", http_404: 2, http_200: 190, http_302: 100},
+        {date: "12/28/2012", http_404: 2, http_200: 10, http_302: 100},
+        {date: "12/29/2012", http_404: 1, http_200: 300, http_302: 200},
+        {date: "12/30/2012", http_404: 2, http_200: 90, http_302: 0},
+        {date: "12/31/2012", http_404: 2, http_200: 90, http_302: 0},
+        {date: "01/01/2013", http_404: 2, http_200: 90, http_302: 0},
+        {date: "01/02/2013", http_404: 1, http_200: 10, http_302: 1},
+        {date: "01/03/2013", http_404: 2, http_200: 90, http_302: 0},
+        {date: "01/04/2013", http_404: 2, http_200: 90, http_302: 0},
+        {date: "01/05/2013", http_404: 2, http_200: 90, http_302: 0},
+        {date: "01/06/2013", http_404: 2, http_200: 200, http_302: 1},
+        {date: "01/07/2013", http_404: 1, http_200: 200, http_302: 100}
+    ];
+
+    var ndx = crossfilter(data);
+
+    
+
+    function print_filter(filter){
+        var f=eval(filter);
+        if (typeof(f.length) != "undefined")
+        {
+
+        }else
+        {
+
+        }
+        if (typeof(f.top) != "undefined")
+        {
+            f=f.top(Infinity);
+        }else
+        {
+
+        }
+        if (typeof(f.dimension) != "undefined")
+        {
+            f=f.dimension(function(d) {
+                return "";
+            }).top(Infinity);
+        }else
+        {
+
+        }
+        console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+    }
+
+
+
+
 });
+
+
+
+
 
 var initializeWidgets = function () {
 
