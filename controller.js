975e595093e330d43a4574cfc4e2f2ca2a964e54
Alejandro <alejandro.saura.villanueva@gmail.com>
2015-03-03 12:49:34 +0100

    - AdminLTE updated to 2.0 - leaflet-map is now a Polymer element - dc-pie-chart is now a polymer element - SPARQL query editor page not working yet

---
+++ controller.js
@@ -0,0 +1,150 @@
+/**
+ * Created by asaura on 27/02/15.
+ */
+
+var rawData = [];
+
+$( document ).ready(function() {
+
+    //execute required queries:
+    getPolygonsFromEuro();
+
+    //initialize widgets:
+    initializeWidgets();
+
+
+
+
+
+
+
+
+});
+
+var initializeWidgets = function () {
+
+    //render the loading screen for each widget (they must have the 'widget' class in their outer div):
+    $(".widget").append('<div class="overlay"><i class="fa fa-refresh fa-spin"></i></div>');
+
+};
+
+var newDataReceived = function () {
+
+    //Delete loading screen
+    $(".overlay").remove();
+    $(".loading-img").remove();
+
+    var ndx = crossfilter(rawData);
+    //add "total" filter as temporal value for cloropleth
+    var idGen = 0
+    rawData.forEach(function(d) {
+        d.total= 1;
+        d.id = idGen;
+        idGen++;
+    });
+    var designationDim = ndx.dimension(function(d) {return d.designation.value;});
+    var numDesignations = designationDim.group().reduceCount();
+    var idDim = ndx.dimension(function(d) {return d.id;});
+    var numId = idDim.group().reduceCount();
+
+    var idDim2 = ndx.dimension(function(d) {return d.id;});
+    var numId2 = idDim.group().reduceCount();
+
+    //These four lines will have to be done attending to the settings section of the object
+    //If not set yet, initialize with default options (first dimension and group of the first crossfilter object)
+
+    //var dcElements = $(".dc-element");
+    //for (var i = 0; i < dcElements.length; i++) {
+    //    dcElements[i].crossfilter = ndx;
+    //    dcElements[i].dimension = designationDim;
+    //    dcElements[i].group = numDesignations;
+    //    dcElements[i].geoJSON = rawData;
+    //    dcElements[i].init();
+    //}
+    // NOT YET
+
+    var dcElements = $(".dc-element");
+    for (var i = 0; i < dcElements.length; i++) {
+
+        if (dcElements[i].tagName == "LEAFLET-MAP")
+        {
+            dcElements[i].crossfilter = ndx;
+            dcElements[i].dimension = idDim;
+            dcElements[i].group = numId;
+            dcElements[i].geoJSON = rawData;
+        };
+        if (dcElements[i].tagName == "PIE-CHART")
+        {
+            dcElements[i].crossfilter = ndx;
+            dcElements[i].dimension = designationDim;
+            dcElements[i].group = numDesignations;
+            dcElements[i].geoJSON = rawData;
+        };
+
+        if (dcElements[i].tagName == "DATA-TABLE")
+        {
+            dcElements[i].crossfilter = ndx;
+            dcElements[i].dimension = idDim2;
+            dcElements[i].group = numId2;
+            dcElements[i].geoJSON = rawData;
+
+            //DataTables is a JQuery plugin that offers problems when used inside Polymer.
+            //This is a common issue and will be solved in future JQuery releases.
+           var  datatable = $("#table").dataTable({
+
+                "bDeferRender": true,
+                // Restricted data in table to 10 rows, make page load faster
+                // Make sure your field names correspond to the column headers in your data file. Also make sure to have default empty values.
+                "aaData": dcElements[i].dimension.top(10),
+
+                "bDestroy": true,
+                "aoColumns": [
+                    { "mData": "name.value", "sDefaultContent": " " },
+                    { "mData": "designation.value", "sDefaultContent": " " }
+                ]
+            });
+
+        };
+
+        dcElements[i].init();
+    }
+
+
+
+
+
+
+    dc.renderAll();
+};
+
+
+//Smart Open Data Query
+var getPolygonsFromEuro = function () {
+
+    var polygonsfeuro_query = 'PREFIX drf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX j.0: <http://inspire.jrc.ec.europa.eu/schemas/gn/3.0/> PREFIX j.1: <http://inspire.jrc.ec.europa.eu/schemas/ps/3.0/> PREFIX j.2: <http://inspire.jrc.ec.europa.eu/schemas/base/3.2/> PREFIX j.3: <http://www.opengis.net/ont/geosparql#> SELECT * WHERE { SERVICE <http://localhost:3030/slovakia/query> { ?res j.3:hasGeometry ?fGeom . ?fGeom j.3:asWKT ?fWKT . ?res j.1:siteProtectionClassification ?spc . ?res j.1:LegalFoundationDate ?lfd . ?res j.1:LegalFoundationDocument ?lfdoc . ?res j.1:inspireId ?inspire . ?res j.1:siteName ?sitename . ?sitename j.0:GeographicalName ?gname . ?gname j.0:spelling ?spelling . ?spelling j.0:SpellingOfName ?spellingofname . ?spellingofname j.0:text ?name . ?inspire j.2:namespace ?namespace . ?inspire j.2:namespace ?localId . ?res j.1:siteDesignation ?siteDesignation . ?siteDesignation j.1:percentageUnderDesignation ?percentageUnderDesignation . ?siteDesignation j.1:designation ?designation . ?siteDesignation j.1:designationScheme ?designationScheme . } } LIMIT 100';
+    var temporal = 'http://demos.gsi.dit.upm.es/fuseki/slovakia/query?query=' + encodeURIComponent(polygonsfeuro_query);
+    var req = new XMLHttpRequest();
+    req.open("GET", temporal, true);
+    var params = encodeURIComponent(polygonsfeuro_query);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.setRequestHeader("Accept", "application/sparql-results+json");
+    //req.setRequestHeader("Content-length", params.length);
+    //req.setRequestHeader("Connection", "close");
+    req.send();
+    console.log("polygons query sent");
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.log("polygons query response received");
+                var res = eval("(" + req.responseText + ")");
+                var data = JSON.stringify(res.results.bindings);
+                rawData = JSON.parse(data);
+
+                newDataReceived();
+
+            } else {
+            }
+        }
+    };
+    return false;
+};
