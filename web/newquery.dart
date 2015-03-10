a54e29598f3ee4b9aacfcfc6125b1dcaef91369b
Enrique Conde <enriqueconde93@gmail.com>
2015-03-10 13:14:05 +0100

    - Theme - Dart - New Query - Footer

---
+++ web/newquery.dart
@@ -0,0 +1,52 @@
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
+import 'dart:html';
+import 'dart:convert';
+
+@Injectable()
+class Query {
+  final String STORAGE_KEY = 'querysSaved';
+  static Storage localStorage = window.localStorage;
+  String name = '';
+  String query = '';
+  var input = querySelector('#queryNameSelector');
+  var myEl2 = querySelector('#querySelector');
+  
+  String toJsonString() {
+      Map data = {"Querys": [
+          {
+              "Name" : name,
+              "Query" : myEl2.value
+          }]};
+      return JSON.encode(data);
+  }
+  
+  void clearFilters() {
+    input.value = "";
+    myEl2.value = "";
+  }
+  
+  void saveData(){
+    List querys = [];
+    if ( window.localStorage.containsKey(STORAGE_KEY)){
+      querys = JSON.decode(window.localStorage[STORAGE_KEY]);
+
+      if(querys == null) {
+        querys = [];
+      }
+    }
+
+    var queryVar = {
+      "Name" : name,
+      "Query" : myEl2.value
+    };
+    querys.add(queryVar);
+    window.localStorage[STORAGE_KEY] = JSON.encode(querys);
+  }
+
+}
+void main() {
+  applicationFactory()
+    .rootContextType(Query)
+    .run();
+}
