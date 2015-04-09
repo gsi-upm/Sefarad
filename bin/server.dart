92cc7acae5d87b1b0608ce4a226e4f106dd26c8f
Enrique Conde <enriqueconde93@gmail.com>
2015-04-09 09:36:24 +0200

    - HTTP server created - Data are saved in json instead of local storage

---
+++ bin/server.dart
@@ -0,0 +1,120 @@
+import 'dart:io';
+import 'package:mongo_dart/mongo_dart.dart';
+
+/* A simple web server that responds to **ALL** GET requests by returning
+ * the contents of data.json file, and responds to ALL **POST** requests
+ * by overwriting the contents of the data.json file
+ *
+ * Browse to it using http://localhost:8080
+ *
+ * Provides CORS headers, so can be accessed from any other page
+ */
+
+final HOST = "127.0.0.1"; // eg: localhost
+final PORT = 8080;
+String url = "../web";
+Db db = new Db("mongodb://127.0.0.1/test");
+DbCollection coll;
+ObjectId id;
+
+void main() {
+  HttpServer.bind(HOST, PORT).then((server) {
+    server.listen((HttpRequest request) {
+      switch (request.method) {
+        case "GET":
+          handleGet(request);
+          break;
+        case "POST":
+          handlePost(request);
+          break;
+        case "OPTIONS":
+          handleOptions(request);
+          break;
+        default: defaultHandler(request);
+      }
+    },
+    onError: printError);
+
+    print("Listening for GET and POST on http://$HOST:$PORT");
+  },
+  onError: printError);
+}
+
+/**
+ * Handle GET requests by reading the contents of data.json
+ * and returning it to the client
+ */
+void handleGet(HttpRequest req) {
+  HttpResponse res = req.response;
+  print("${req.method}: ${req.uri.path}");
+  String data_file = url + req.uri.path + ".json";
+  addCorsHeaders(res);
+
+  var file = new File(data_file);
+  if (file.existsSync()) {
+    res.headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    file.readAsBytes().asStream().pipe(res); // automatically close output stream
+  }
+  else {
+    var err = "Could not find file: $data_file";
+    res.addString(err);
+    res.close();
+  }
+
+}
+
+/**
+ * Handle POST requests by overwriting the contents of data.json
+ * Return the same set of data back to the client.
+ */
+void handlePost(HttpRequest req) {
+  HttpResponse res = req.response;
+  print("${req.method}: ${req.uri.path}");
+  String data_file = url + req.uri.path + ".json";
+
+  addCorsHeaders(res);
+
+  req.listen((List<int> buffer) {
+    var file = new File(data_file);
+    var ioSink = file.openWrite(); // save the data to the file
+    ioSink.add(buffer);
+    ioSink.close();
+
+    // return the same results back to the client
+    res.add(buffer);
+    res.close();
+  },
+  onError: printError);
+
+}
+
+/**
+ * Add Cross-site headers to enable accessing this server from pages
+ * not served by this server
+ *
+ * See: http://www.html5rocks.com/en/tutorials/cors/
+ * and http://enable-cors.org/server.html
+ */
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+
+void handleOptions(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  print("${req.method}: ${req.uri.path}");
+  res.statusCode = HttpStatus.NO_CONTENT;
+  res.close();
+}
+
+void defaultHandler(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  res.statusCode = HttpStatus.NOT_FOUND;
+  res.addString("Not found: ${req.method}, ${req.uri.path}");
+  res.close();
+}
+
+void printError(error) => print(error);
