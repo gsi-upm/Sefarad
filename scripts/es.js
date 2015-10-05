26d76cbefe44e6220d2fa872b22dd7e85080fcba
enriquecs <enriqueconde93@gmail.com>
2015-10-05 09:26:57 +0200

    ldf-client added

--- scripts/es.js
+++ scripts/es.js
@@ -1,48 +1,37 @@
-/*var client = new $.es.Client({
+var client = new $.es.Client({
   hosts: 'localhost:9200'
 });
-client.search({
-  // undocumented params are appended to the query string
-  index: 'bank',
-  type: 'account',
-  body: {
-    query: { 
-	"match_all": {
-	} 
-    }
-  }
-}).then(function (resp) {
-  var resultsDough = [];
-  var resultsPie = [];
-  var hits = resp.hits.hits;
-  console.log(hits);
-  hits.forEach(function(entry) {
-  	resultsDough.push(entry._source.balance);
-	resultsPie.push(entry._source.age);
-  });
-  document.querySelector('chart-doughnut').setAttribute('values','['+resultsDough+']');
-  document.querySelector('chart-pie').setAttribute('values','['+resultsPie+']');
-}, function (error) {
-  console.trace(error.message);
-});
+
 client.search({
   // undocumented params are appended to the query string
   index: 'twitter',
-  type: 'tweets',
+  type: 'tweet',
   body: {
     query: { 
-	"match_all": {
-	} 
+  "match_all": {
+     } 
     }
   }
 }).then(function (resp) {
   var hits = resp.hits.hits;
-  var resultsTweet = [];
-  console.log(hits);
   hits.forEach(function(entry) {
-	resultsTweet.push(entry._source.favorite_count);
+    console.log(entry._source.text);
   });
-  document.querySelector('#favs').setAttribute('values','['+resultsTweet+']');
-}, function (error) {
-  console.trace(error.message);
-});*/
+});
+
+document.addEventListener('WebComponentsReady', function() {
+        
+        var ldfClientStreaming = document.querySelector('#ldf-client-streaming');
+        // Process data as it appears
+        ldfClientStreaming.addEventListener('ldf-query-streaming-response-partial',
+            function(e) {
+          var pre = document.createElement('pre');
+          pre.classList.add('streaming');
+          pre.textContent = JSON.stringify(e.detail.response);
+          document.querySelector('#td2').appendChild(pre);
+        });
+        // Get notified once all data is received
+        ldfClientStreaming.addEventListener('ldf-query-streaming-response-end', function() {
+          alert('Received all data');
+        });
+      });
